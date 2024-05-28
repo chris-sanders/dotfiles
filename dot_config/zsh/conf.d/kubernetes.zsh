9356445ccb764e50cd842c90f33c4fb7ebb63a26
Chris Sanders <sanders.chris@gmail.com>
2024-05-28 21:45:47 +0000

    Updated configuration

--- dot_config/zsh/conf.d/kubernetes.zsh
+++ dot_config/zsh/conf.d/kubernetes.zsh
@@ -1,36 +1,3 @@
-# Directory containing kubeconfig files
-KUBECONFIG_DIR=~/.config/kube/configs.d/
-
-# Function to load all kubeconfig files
-load_kubeconfigs() {
-  export KUBECONFIG=$(find "$KUBECONFIG_DIR" -type f | tr '\n' ':')
-}
-
-# Initially load all configs
-load_kubeconfigs
-
-# Function to set kubeconfig per shell by filename
-set_kubeconfig() {
-  local config_file="$KUBECONFIG_DIR$1"
-  if [[ -f "$config_file" ]]; then
-    export KUBECONFIG="$config_file"
-    export KUBE_CONTEXT=$(kubectl config current-context)
-  else
-    echo "Kubeconfig file '$config_file' not found"
-  fi
-}
-
-# Function to list all available kubeconfig files and indicate the active one
-list_kubeconfigs() {
-  for file in "$KUBECONFIG_DIR"*; do
-    if [[ "$file" == "$KUBECONFIG" ]]; then
-      echo "* $(basename "$file") (active)"
-    else
-      echo "  $(basename "$file")"
-    fi
-  done
-}
-
 # Powerlevel10k customization to show Kubernetes context
 typeset -g POWERLEVEL9K_KUBECONTEXT_SHOW_ON_COMMAND='kubectl'
 POWERLEVEL9K_KUBECONTEXT_CONTENT_EXPANSION='${KUBE_CONTEXT}'
@@ -42,3 +9,40 @@ if command -v kubectl > /dev/null 2>&1; then
   alias k=kubectl
   compdef _kubectl k
 fi
+
+# Directory containing kubeconfig files
+KUBECONFIG_DIR=~/.config/kube/configs.d/
+
+# Check if kubeconfig directory exists
+if [[ -d "$KUBECONFIG_DIR" ]]; then
+  # Function to load all kubeconfig files
+  load_kubeconfigs() {
+    export KUBECONFIG=$(find "$KUBECONFIG_DIR" -type f | tr '\n' ':')
+  }
+
+  # Initially load all configs
+  load_kubeconfigs
+
+  # Function to set kubeconfig per shell by filename
+  set_kubeconfig() {
+    local config_file="$KUBECONFIG_DIR$1"
+    if [[ -f "$config_file" ]]; then
+      export KUBECONFIG="$config_file"
+      export KUBE_CONTEXT=$(kubectl config current-context)
+    else
+      echo "Kubeconfig file '$config_file' not found"
+    fi
+  }
+
+  # Function to list all available kubeconfig files and indicate the active one
+  list_kubeconfigs() {
+    for file in "$KUBECONFIG_DIR"*; do
+      if [[ "$file" == "$KUBECONFIG" ]]; then
+        echo "* $(basename "$file") (active)"
+      else
+        echo "  $(basename "$file")"
+      fi
+    done
+  }
+fi
+
